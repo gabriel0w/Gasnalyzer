5370456ba8a845938b17a3a97a60a96a735e41d4
gabriel0w <gabrielo.dzn@gmail.com>
2023-11-14 06:28:37 -0300

    chore : update routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const TestController = require('@controller/test.js');
 const authController = require('./controller/authcontroller');
+const UserController = require('./controller/userController');
+const DataController = require('./controller/datacontroller');
 
 router.get('/', TestController.helloworld);
 
@@ -15,5 +17,9 @@ router.post('/request-reset-password', authController.requestResetPassword);
 
 // Rota para redefinir senha
 router.post('/reset-password', authController.resetPassword);
+
+router.post('/users', UserController.createUser);
+
+router.get('/data/before/:date', DataController.getDataBefore);
 
 module.exports = router;
