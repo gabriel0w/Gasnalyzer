ce721f12cf5bc24b48e4a45d9b9c049ecaf5288e
gabriel0w <gabrielo.dzn@gmail.com>
2023-11-14 06:33:48 -0300

    feat : update routes and authenticate syncfusion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import MainContent from './components/maincontent';
 import LoginPage from './components/loginpage'; // Seu componente de página de login
+import ForgotPasswordPage from './components/forgotpasswordpage';
+import RegisterPage from './components/registerpage';
+import ResetPasswordPage from './components/resetpasswordpage';
+import ChartsPage from './components/chartpage';
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
         <Routes>
           <Route path="/" element={<MainContent />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password/:resetToken" element={<ResetPasswordPage />} />
+          <Route path="/charts" element={<ChartsPage />} />
           {/* Adicione outras rotas conforme necessário */}
         </Routes>
       </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { enableRipple } from '@syncfusion/ej2-base';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense(process.env.REACT_APP_SYNCFUSION_LICENSE_KEY);
+enableRipple(true);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
